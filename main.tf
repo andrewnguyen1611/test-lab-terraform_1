5a2d65192419feb84f63689816c1237744ad1a6c
andrewnguyen1611 <an.ntt@outlook.com.vn>
2023-10-30 10:18:02 +0700

    update main

--- main.tf
+++ main.tf
@@ -19,28 +19,19 @@ provider "aws" {
         enable_dns_hostnames = true
 
         tags = {
-          "Name" = "vpc1"
+          "Name" = "VLU_VPC"
         }
       }
 
-# resource "aws_vpc" "my_vpc_2" {
-#   cidr_block           = "20.0.0.0/16"
-#   enable_dns_hostnames = true
 
-#   tags = {
-#     "Name" = "vpc2"
-#   }
-# }
-
-
-# tạo subnet cho vcp 1
+# tạo subnet 
         variable "name_pri_sub" {
-        default = ["private-subnet-01","private-subnet-02","private-subnet-03","private-subnet-04"]
+        default = ["VLU_private-subnet-01","VLU_private-subnet-02","VLU_private-subnet-03","VLU_private-subnet-04"]
         type = list(string)
         }
 
         variable "name_pub_sub" {
-        default = ["public-subnet-01","public-subnet-02"]
+        default = ["VLU_public-subnet-01","VLU_public-subnet-02"]
         type = list(string)
         }
 
@@ -53,7 +44,6 @@ provider "aws" {
 
         resource "aws_subnet" "private_subnet" {
           count = length(local.private)
-
           vpc_id            = aws_vpc.my_vpc_1.id
           cidr_block        = local.private[count.index]
           availability_zone = local.zone[count.index % length(local.zone)]
@@ -74,45 +64,53 @@ provider "aws" {
           }
         }
 
-
-
-
-#         # tạo subnet cho vpc2 
-#         # locals {
-#         #   public2 = ["20.0.1.0/24"]
-#         #   zone2   = ["us-east-1a"]
-#         # }
-#         # resource "aws_subnet" "public_subnet_2" {
-#         #   vpc_id            = aws_vpc.my_vpc_2.id
-#         #   cidr_block        = local.public2.id
-#         #   availability_zone = local.zone2.id
-
-#         #   tags = {
-#         #     "Name" = "public-subnet_vcp2"
-#         #   }
-#         # }
-
+#tạo security
+        resource "aws_security_group" "SG" {
+          vpc_id = aws_vpc.my_vpc_1.id
+          name   = "Web Server Security Group"
+          ingress {
+            protocol    = "tcp"
+            from_port   = 80
+            to_port     = 80
+            cidr_blocks = ["0.0.0.0/0"]
+          }
+          egress {
+            protocol    = "tcp"
+            from_port   = 0
+            to_port     = 0
+            cidr_blocks = ["0.0.0.0/0"]
+          }
+           tags = {
+                    "Name" = "VLU_securitygroup"
+        }
+        }
 
 # # tạo internet gateway
         resource "aws_internet_gateway" "ig" {
           vpc_id = aws_vpc.my_vpc_1.id
 
           tags = {
-            "Name" = "ig1"
+            "Name" = "VLU_ig1"
           }
         }
 
-# resource "aws_internet_gateway" "ig2" {
-#   vpc_id = aws_vpc.my_vpc_2.id
+#tạo natgateway
 
-#   tags = {
-#     "Name" = "ig2"
-#   }
-# }
+        resource "aws_eip" "nat" {
+          vpc = true
+        }
 
+        resource "aws_nat_gateway" "public" {
+          depends_on = [aws_internet_gateway.ig]
+          allocation_id = aws_eip.nat.id
+          subnet_id     = aws_subnet.public_subnet[0].id
+          tags = {
+            Name = "Public NAT"
+          }
+        }
 
 # #tạo routetable
-        resource "aws_route_table" "public1" {
+        resource "aws_route_table" "public" {
           vpc_id = aws_vpc.my_vpc_1.id
 
           route {
@@ -139,81 +137,17 @@ provider "aws" {
                   }
                 }
 
+resource "aws_route_table_association" "public_association" {
+     # subnet_id = aws_subnet.public_subnet[count.index]
+     for_each       = { for k, v in aws_subnet.public_subnet : k => v }
+    subnet_id      = each.value.id
+      route_table_id = aws_route_table.public.id
+  }
 
-#     resource "aws_route_table_association" "for_public" {
-#          subnet_id      = [aws_subnet.public_subnet.id]
-#         route_table_id = aws_route_table.public1.id
-#     }
-
-# resource "aws_route_table_association" "for_private" {
-#          subnet_id      = [aws_subnet.private_subnet.id]
-#         route_table_id = aws_route_table.private.id
-#     }
-
-# resource "aws_route_table" "public2" {
-#   vpc_id = aws_vpc.my_vpc_2.id
-
-#   route {
-#     cidr_block = "0.0.0.0/0"
-#     gateway_id = aws_internet_gateway.ig2.id
-#   }
-
-#   tags = {
-#     "Name" = "public"
-#   }
-# }
-
-
-#tạo natgateway
-
-        resource "aws_eip" "nat" {
-          vpc = true
-        }
-
-        resource "aws_nat_gateway" "public" {
-          depends_on = [aws_internet_gateway.ig]
-          allocation_id = aws_eip.nat.id
-          subnet_id     = aws_subnet.public_subnet[0].id
-          tags = {
-            Name = "Public NAT"
-          }
-        }
-
-
-       #tạo loadbalacing
-        resource "aws_security_group" "webapp" {
-          vpc_id = aws_vpc.my_vpc_1.id
-          name   = "Web Server Security Group"
-          ingress {
-            protocol    = "tcp"
-            from_port   = 80
-            to_port     = 80
-            cidr_blocks = ["0.0.0.0/0"]
-          }
-          egress {
-            protocol    = "tcp"
-            from_port   = 0
-            to_port     = 0
-            cidr_blocks = ["0.0.0.0/0"]
-          }
-        }
-
-
-
-# tạo ec2
-      resource "aws_instance" "web" {
-        ami                    = "ami-0b89f7b3f054b957e"
-        instance_type          = "t2.micro"
-        availability_zone      = local.zone[1]
-        vpc_security_group_ids = [aws_security_group.webapp.id]
-        subnet_id              = aws_subnet.public_subnet[1].id
-        associate_public_ip_address = true
-        tags = {
-          "Name" = "web"
-        }
-      }
-
-#autoscaling
-
-
+  resource "aws_route_table_association" "private_association" {
+     # subnet_id = aws_subnet.private_subnet[count.index]\
+     for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+    subnet_id      = each.value.id
+      route_table_id = aws_route_table.private.id
+  }
 
